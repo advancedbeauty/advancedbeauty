b3da63fb8b4e0810f6bd4e3f1d87427ef3786211
diwakarjha <diwakarjha554@gmail.com>
2025-02-22 09:15:13 +0530

    added frontend component

--- apps/web/components/footer/footer-bar/index.tsx
+++ apps/web/components/footer/footer-bar/index.tsx
@@ -12,7 +12,7 @@ const FooterBar = () => {
     return (
         <div className='fixed bottom-0 bg-[#111111] w-full z-30 px-3 py-2 border-t-[0.1px] border-gray-500 text-white lg:hidden flex justify-around'>
             <IconLink activeIcon={GoHomeFill} icon={GoHome} href='/' text={'Home'}/>
-            <IconLink activeIcon={PiSquaresFourFill } icon={PiSquaresFour} href='/categories' text={'Categories'}/>
+            {/* <IconLink activeIcon={PiSquaresFourFill } icon={PiSquaresFour} href='/categories' text={'Categories'}/> */}
             <IconLink activeIcon={IoIosHeart} icon={IoIosHeartEmpty} href='/wishlist' text={'Wishlist'}/>
             <IconLink activeIcon={RiAccountCircleFill} icon={RiAccountCircleLine} href='/auth' text={'Account'}/>
             <IconLink activeIcon={BsHandbagFill} icon={BsHandbag} href='/cart' text={'Cart'}/>
